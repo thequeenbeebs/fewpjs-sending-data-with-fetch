e6b1170e80dd9c6847fba89e6762d740b476878f
Blaire Baker <bakerblaire@gmail.com>
2021-01-13 17:47:21 -0600

    commit tree a37519cb36ce89b8b938c0c0f193ef71938d4b83

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+const body = document.querySelector('body')
+
+function submitData(userName, userEmail) {
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }
+
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(object => {
+            let id = document.createElement('p')
+            id.innerHTML = object.id
+            body.appendChild(id)
+        })
+        .catch(error => {
+            let message = document.createElement('p')
+            message.innerHTML = error.message
+            body.appendChild(message)
+        })
+}
+
+
